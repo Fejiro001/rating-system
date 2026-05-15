24e897463d29a6200488b8d948333547190a95d2
Fejiro001 <aberefejiro@gmail.com>
2026-05-15 11:35:35 -0500

    Created a ratings array containing index and rating text

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,4 +1,14 @@
-function StarRating() {
+import Star from "./Star";
+
+function StarRating(props) {
+  const ratings = [
+    { index: 0, rating: "Terrible" },
+    { index: 1, rating: "Bad" },
+    { index: 2, rating: "Okay" },
+    { index: 3, rating: "Good" },
+    { index: 4, rating: "Excellent" }
+  ];
+
   return <></>;
 }
 
