a3f8731073710299dae09ed524bd9a3b0f84832f
Fejiro001 <aberefejiro@gmail.com>
2026-05-15 13:49:09 -0500

    Added state for star hover and click

--- src/components/Star.js
+++ src/components/Star.js
@@ -1,8 +1,10 @@
 import { FaRegStar } from "react-icons/fa6";
 
 function Star(props) {
+  const { onHover, handleRating } = props;
+
   return (
-    <button>
+    <button onClick={handleRating} onMouseEnter={onHover}>
       <FaRegStar />
     </button>
   );
--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import Star from "./Star";
 
 function StarRating(props) {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+
   const ratings = [
     { index: 0, rating: "Terrible" },
     { index: 1, rating: "Bad" },
@@ -9,7 +13,21 @@ function StarRating(props) {
     { index: 4, rating: "Excellent" }
   ];
 
-  return <></>;
+  const handleRating = (e) => {
+    console.log(e.target);
+  };
+
+  const handleButtonHover = () => {};
+
+  return (
+    <>
+      <h1>Rate our Product</h1>
+      {ratings.map((rate) => (
+        <Star isHovered={isHovered} onHover={handleButtonHover} handleRating={handleRating} key={rate.index} />
+      ))}
+      <h2>Good</h2>
+    </>
+  );
 }
 
 export default StarRating;
